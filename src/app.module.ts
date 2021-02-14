297306e20737a438ace40c1acc757a2e5dd1c0f5
jungsikyeo <mdotdom12@naver.com>
2021-02-14 22:35:10 +0900

    ssl....

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,28 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
-import * as Joi from 'joi';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GraphQLModule } from '@nestjs/graphql';
-import { PodcastsModule } from './podcast/podcasts.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Podcast } from './podcast/entities/podcast.entity';
-import { Episode } from './podcast/entities/episode.entity';
-import { UsersModule } from './users/users.module';
-import { JwtModule } from './jwt/jwt.module';
-import { AuthModule } from './auth/auth.module';
-import { User } from './users/entities/user.entity';
-import { JwtMiddleware } from './jwt/jwt.middleware';
-import { Review } from './podcast/entities/review.entity';
-import { ConfigModule } from '@nestjs/config';
+import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
+import * as Joi from "joi";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { GraphQLModule } from "@nestjs/graphql";
+import { PodcastsModule } from "./podcast/podcasts.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Podcast } from "./podcast/entities/podcast.entity";
+import { Episode } from "./podcast/entities/episode.entity";
+import { UsersModule } from "./users/users.module";
+import { JwtModule } from "./jwt/jwt.module";
+import { AuthModule } from "./auth/auth.module";
+import { User } from "./users/entities/user.entity";
+import { JwtMiddleware } from "./jwt/jwt.middleware";
+import { Review } from "./podcast/entities/review.entity";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "production",
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'production').required(),
+        NODE_ENV: Joi.string().valid("dev", "production").required(),
         DB_HOST: Joi.string(),
         DB_PORT: Joi.string(),
         DB_USERNAME: Joi.string(),
@@ -32,23 +32,34 @@ import { ConfigModule } from '@nestjs/config';
       }),
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: "postgres",
       ...(process.env.DATABASE_URL
-          ? { url: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
-          : {
+        ? {
+            url: process.env.DATABASE_URL,
+            extra: {
+              ssl: { rejectUnauthorized: false },
+            },
+          }
+        : {
             host: process.env.DB_HOST,
             port: +process.env.DB_PORT,
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
+            extra: {
+              ssl: { rejectUnauthorized: false },
+            },
           }),
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging: process.env.NODE_ENV !== 'production',
+      synchronize: process.env.NODE_ENV !== "production",
+      logging: process.env.NODE_ENV !== "production",
       entities: [Podcast, Episode, User, Review],
     }),
     GraphQLModule.forRoot({
+      //playground: process.env.NODE_ENV !== 'production',
+      playground: true,
+      introspection: true,
       autoSchemaFile: true,
-      context: ({ req }) => ({ user: req['user'] }),
+      context: ({ req }) => ({ user: req["user"] }),
     }),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
@@ -63,7 +74,7 @@ import { ConfigModule } from '@nestjs/config';
 export class AppModule {
   configure(consumer: MiddlewareConsumer): any {
     consumer
-        .apply(JwtMiddleware)
-        .forRoutes({ path: '/graphql', method: RequestMethod.POST });
+      .apply(JwtMiddleware)
+      .forRoutes({ path: "/graphql", method: RequestMethod.POST });
   }
 }
